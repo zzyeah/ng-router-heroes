9a12013bf8bb846c2c9f7afd45dad2cb8b1d0104
zzyeah <541767316@qq.com>
2021-06-22 17:57:45 +0800

    router-note1

---
+++ src/app/c-routing/c-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CRoutingRoutingModule } from './c-routing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CRoutingRoutingModule
+  ]
+})
+export class CRoutingModule { }
